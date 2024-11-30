414d03e649971dc938ad81cd67680585dcadf107
Amy Lau <tszyanalau@gmail.com>
2024-12-01 02:08:28 +0900

    style: add bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import './assets/styles/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
